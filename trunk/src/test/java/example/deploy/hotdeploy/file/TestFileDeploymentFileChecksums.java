46a8df80a5e77b703245eb1d1406d1071c8560a0
Anders Appelqvist <anders.appelqvist@atex.com>
2011-06-23 09:58:24 +0000

    rollback som changes

--- trunk/src/test/java/example/deploy/hotdeploy/file/TestFileDeploymentFileChecksums.java
+++ trunk/src/test/java/example/deploy/hotdeploy/file/TestFileDeploymentFileChecksums.java
@@ -2,11 +2,11 @@ package example.deploy.hotdeploy.file;
 
 import java.io.File;
 
-public class TestFileDeploymentFileChecksums /* extends ChecksumTestCase */{ 
-    /*
+public class TestFileDeploymentFileChecksums extends ChecksumTestCase{ 
+    
     @Override
     protected DeploymentFile getFile(String fileName) {
         return new FileDeploymentFile(
                 new File(EQUAL_FILES_DIRECTORY + fileName));
-		}*/
+		}
 }
